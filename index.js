78713dff7f7f89f4a8f3839c6046e6d4c197e3cd
FormatCom <formatcomvinicio@gmail.com>
2016-02-20 22:40:17 -0500

    se creo eventos y metodos para el manejo de datos

--- index.js
+++ index.js
@@ -5,19 +5,153 @@
     if (obj == null) {
       obj = {};
     }
-    this._data = Object.freeze(obj);
+    this.objects = obj.json ? Object.freeze(obj.json()) : Object.freeze(obj);
+    this._init();
     return this;
   };
 
   Block.prototype = {
-    get: function() {
-      return new Block(this.json());
-    },
-    set: function(callback) {
-      return new Block(callback(this.json()));
+    _init: function() {
+      var self;
+      self = this;
+      this.all = function() {
+        var _obj;
+        _obj = new Block(this.all.init(this.json()) || this);
+        _obj = new Block(this.all.finish(_obj.json()) || this);
+        _obj.all.init = this.all.init;
+        _obj.all.finish = this.all.finish;
+        return _obj;
+      };
+      this.all.init = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj.all.init = event;
+        }
+        return _obj;
+      };
+      this.all.finish = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj.all.finish = event;
+        }
+        return _obj;
+      };
+      this.set = function(callback) {
+        var _obj;
+        _obj = new Block(this.set.init(this.json()) || this);
+        _obj = new Block(callback(_obj.json()) || this);
+        _obj = new Block(this.set.finish(_obj.json()) || this);
+        _obj.set.init = this.set.init;
+        _obj.set.finish = this.set.finish;
+        return _obj;
+      };
+      this.set.init = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj.set.init = event;
+        }
+        return _obj;
+      };
+      this.set.finish = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj.set.finish = event;
+        }
+        return _obj;
+      };
+      this["delete"] = function(obj) {
+        var _obj;
+        if (obj == null) {
+          obj = {};
+        }
+        _obj = new Block(this["delete"].init(this.json()) || this);
+        _obj = _obj.json().filter(function(item) {
+          var i, key, len, ref, valid;
+          valid = true;
+          ref = Object.keys(obj);
+          for (i = 0, len = ref.length; i < len; i++) {
+            key = ref[i];
+            valid = item[key] !== obj[key];
+            if (!valid) {
+              break;
+            }
+          }
+          if (valid) {
+            return item;
+          }
+        });
+        _obj = new Block(_obj);
+        _obj = new Block(this["delete"].finish(_obj.json()) || this);
+        _obj["delete"].init = this["delete"].init;
+        _obj["delete"].finish = this["delete"].finish;
+        return _obj;
+      };
+      this["delete"].init = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj["delete"].init = event;
+        }
+        return _obj;
+      };
+      this["delete"].finish = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj["delete"].finish = event;
+        }
+        return _obj;
+      };
+      this.find = function(obj) {
+        var _obj;
+        if (obj == null) {
+          obj = {};
+        }
+        _obj = new Block(this.find.init(this.json()) || this);
+        _obj = _obj.json().filter(function(item) {
+          var i, key, len, ref, valid;
+          valid = true;
+          ref = Object.keys(obj);
+          for (i = 0, len = ref.length; i < len; i++) {
+            key = ref[i];
+            valid = item[key] === obj[key];
+            if (!valid) {
+              break;
+            }
+          }
+          if (valid) {
+            return item;
+          }
+        });
+        _obj = new Block(_obj);
+        _obj = new Block(this.find.finish(_obj.json()) || this);
+        _obj.find.init = this.find.init;
+        _obj.find.finish = this.find.finish;
+        return _obj;
+      };
+      this.find.init = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj.find.init = event;
+        }
+        return _obj;
+      };
+      return this.find.finish = function(event) {
+        var _obj;
+        _obj = new Block(typeof event === "object" ? event : self);
+        if (typeof event === 'function') {
+          _obj.find.finish = event;
+        }
+        return _obj;
+      };
     },
     json: function() {
-      return JSON.parse(JSON.stringify(this._data));
+      return JSON.parse(JSON.stringify(this.objects));
     },
     equals: function(obj) {
       var i, len, prop, self, type, x;
--- main.coffee
+++ main.coffee
@@ -1,12 +1,138 @@
 Block = ( obj = {} ) ->
-  @_data = Object.freeze obj
+  @objects = if obj.json then Object.freeze obj.json() else Object.freeze obj
+  @_init()
   return @
 
-
 Block.prototype =
-  get: -> new Block (@json())
-  set: (callback) -> new Block callback( @json() )
-  json: -> JSON.parse JSON.stringify @_data
+  _init: () ->
+
+    self = @
+
+    @all = () ->
+      _obj = new Block( @all.init( @json() ) || @ )
+      _obj = new Block( @all.finish( _obj.json() ) || @ )
+      _obj.all.init   = @all.init
+      _obj.all.finish = @all.finish
+      return _obj
+
+    @all.init = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+     
+      if typeof(event) == 'function'
+        _obj.all.init = event
+
+      return _obj
+
+    @all.finish = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+    
+      if typeof(event) == 'function'
+        _obj.all.finish = event
+    
+      return _obj
+
+
+    @set = (callback) ->
+      _obj = new Block( @set.init( @json() ) || @ )
+      _obj = new Block( callback( _obj.json() ) || @ )
+      _obj = new Block( @set.finish( _obj.json() ) || @ )
+      _obj.set.init   = @set.init
+      _obj.set.finish = @set.finish
+
+      return _obj
+
+    @set.init = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+     
+      if typeof(event) == 'function'
+        _obj.set.init = event
+
+      return _obj
+
+    @set.finish = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+    
+      if typeof(event) == 'function'
+        _obj.set.finish = event
+    
+      return _obj
+
+
+    @delete = (obj={}) ->
+      _obj = new Block( @delete.init( @json() ) || @ )
+      _obj = _obj.json().filter (item) ->
+        
+        valid = true
+        
+        for key in Object.keys(obj)
+          valid = item[key] != obj[key]
+          if !valid then break
+
+        if valid then return item
+      
+      _obj = new Block _obj
+
+      _obj = new Block( @delete.finish( _obj.json() ) || @ )
+      _obj.delete.init   = @delete.init
+      _obj.delete.finish = @delete.finish
+
+      return _obj
+
+    @delete.init = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+     
+      if typeof(event) == 'function'
+        _obj.delete.init = event
+
+      return _obj
+
+    @delete.finish = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+    
+      if typeof(event) == 'function'
+        _obj.delete.finish = event
+    
+      return _obj
+
+
+
+    @find = (obj={}) ->
+      _obj = new Block( @find.init( @json() ) || @ )
+      _obj = _obj.json().filter (item) ->
+        
+        valid = true
+        
+        for key in Object.keys(obj)
+          valid = item[key] == obj[key]
+          if !valid then break
+
+        if valid then return item
+      
+      _obj = new Block _obj
+      _obj = new Block( @find.finish( _obj.json() ) || @ )
+      _obj.find.init   = @find.init
+      _obj.find.finish = @find.finish
+
+      return _obj
+
+    @find.init = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+     
+      if typeof(event) == 'function'
+        _obj.find.init = event
+
+      return _obj
+
+    @find.finish = (event) ->
+      _obj = new Block( if typeof(event) == "object" then event else self )
+    
+      if typeof(event) == 'function'
+        _obj.find.finish = event
+    
+      return _obj
+
+
+  json: -> JSON.parse JSON.stringify @objects
   equals: (obj={}) ->
     self = @json()
     x    = if obj.json then obj.json() else obj
