0755dfefe577e58c04414b42317f65c16d05482a
FormatCom <formatcomvinicio@gmail.com>
2016-02-22 11:02:11 -0500

    se agrego soporte a metodos con block

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ module.exports = find;
 
 
 },{"./setting":7}],5:[function(require,module,exports){
-var BASE, Block, Method, SUPPORT, all, del, find, i, len, prop, set;
+var BASE, Block, Method, SUPPORT_IMMUTABLE, SUPPORT_IMMUTABLE_BLOCK, SUPPORT_MUTABLE, SUPPORT_MUTABLE_BLOCK, all, del, find, i, j, k, l, len, len1, len2, len3, prop, set;
 
 Method = require('./Method');
 
@@ -164,7 +164,13 @@ del = require('./delete');
 
 find = require('./find');
 
-SUPPORT = ['push', 'unshift', 'pop', 'shift', 'splice', 'sort'];
+SUPPORT_MUTABLE_BLOCK = ['push', 'unshift'];
+
+SUPPORT_MUTABLE = ['pop', 'shift', 'splice', 'sort', 'reverse'];
+
+SUPPORT_IMMUTABLE_BLOCK = ['concat'];
+
+SUPPORT_IMMUTABLE = ['map', 'filter'];
 
 Block = function(obj) {
   if (obj == null) {
@@ -173,7 +179,8 @@ Block = function(obj) {
   this._objects = obj.json ? Object.freeze(obj.json()) : Object.freeze(obj);
   this.global = new Method(new Function(), 'global', this);
   this.count = 1;
-  this._support = SUPPORT.concat(['map', 'equals', 'json']);
+  this._support = SUPPORT_MUTABLE.concat(SUPPORT_IMMUTABLE.concat(['json']));
+  this._support_block = SUPPORT_MUTABLE_BLOCK.concat(SUPPORT_IMMUTABLE_BLOCK.concat(['equals']));
   this._init();
   return this;
 };
@@ -184,12 +191,6 @@ Block.prototype = {
     this.set = new Method(set, 'set', this);
     this["delete"] = new Method(del, 'delete', this);
     return this.find = new Method(find, 'find', this);
-  },
-  map: function() {
-    var _obj;
-    _obj = this.json();
-    _obj = _obj.map.apply(_obj, arguments);
-    return new Block(_obj);
   },
   json: function(unique) {
     var _obj, keys;
@@ -245,18 +246,63 @@ Block.prototype = {
   }
 };
 
-BASE = function(_prop) {
+BASE = function(_prop, _immutable, block) {
+  if (_immutable == null) {
+    _immutable = false;
+  }
+  if (block == null) {
+    block = true;
+  }
   return function() {
-    var _obj;
+    var _args, _obj, i, index, item, j, key, len, len1, ref, ref1;
     _obj = this.json();
-    _obj[_prop].apply(_obj, arguments);
+    if (block === true) {
+      _args = [];
+      ref = Object.keys(arguments);
+      for (i = 0, len = ref.length; i < len; i++) {
+        key = ref[i];
+        item = arguments[key];
+        if (item.json) {
+          item = item.json();
+          ref1 = Object.keys(item);
+          for (j = 0, len1 = ref1.length; j < len1; j++) {
+            index = ref1[j];
+            _args.push(item[index]);
+          }
+        } else {
+          _args.push(item);
+        }
+      }
+    } else {
+      _args = arguments;
+    }
+    if (_immutable) {
+      _obj = _obj[_prop].apply(_obj, _args);
+    } else {
+      _obj[_prop].apply(_obj, _args);
+    }
     return new Block(_obj);
   };
 };
 
-for (i = 0, len = SUPPORT.length; i < len; i++) {
-  prop = SUPPORT[i];
-  Block.prototype[prop] = BASE(prop);
+for (i = 0, len = SUPPORT_MUTABLE_BLOCK.length; i < len; i++) {
+  prop = SUPPORT_MUTABLE_BLOCK[i];
+  Block.prototype[prop] = BASE(prop, false, true);
+}
+
+for (j = 0, len1 = SUPPORT_MUTABLE.length; j < len1; j++) {
+  prop = SUPPORT_MUTABLE[j];
+  Block.prototype[prop] = BASE(prop, false, false);
+}
+
+for (k = 0, len2 = SUPPORT_IMMUTABLE_BLOCK.length; k < len2; k++) {
+  prop = SUPPORT_IMMUTABLE_BLOCK[k];
+  Block.prototype[prop] = BASE(prop, true, true);
+}
+
+for (l = 0, len3 = SUPPORT_IMMUTABLE.length; l < len3; l++) {
+  prop = SUPPORT_IMMUTABLE[l];
+  Block.prototype[prop] = BASE(prop, true, false);
 }
 
 module.exports = Block;
--- main.coffee
+++ main.coffee
@@ -4,13 +4,17 @@ set     = require './set'
 del     = require './delete'
 find    = require './find'
 
-SUPPORT = ['push', 'unshift', 'pop', 'shift', 'splice', 'sort', 'reverse']
+SUPPORT_MUTABLE_BLOCK   = ['push', 'unshift']
+SUPPORT_MUTABLE         = ['pop', 'shift', 'splice', 'sort', 'reverse']
+SUPPORT_IMMUTABLE_BLOCK = ['concat']
+SUPPORT_IMMUTABLE       = ['map', 'filter']
 
 Block = ( obj = [] ) ->
-  @_objects  = if obj.json then Object.freeze obj.json() else Object.freeze obj
-  @global    = new Method new Function(), 'global', @
-  @count     = 1
-  @._support = SUPPORT.concat ['map', 'equals', 'json']
+  @_objects        = if obj.json then Object.freeze obj.json() else Object.freeze obj
+  @global          = new Method new Function(), 'global', @
+  @count           = 1
+  @._support       = SUPPORT_MUTABLE.concat SUPPORT_IMMUTABLE.concat ['json']
+  @._support_block = SUPPORT_MUTABLE_BLOCK.concat SUPPORT_IMMUTABLE_BLOCK.concat ['equals']
   @_init()
   return @
 
@@ -22,11 +26,6 @@ Block.prototype =
     @set    = new Method set,  'set',    @
     @delete = new Method del,  'delete', @
     @find   = new Method find, 'find',   @
-
-  map: () ->
-    _obj = @json()
-    _obj = _obj.map.apply _obj, arguments
-    new Block _obj
 
   json: (unique=false) ->
     _obj = JSON.parse JSON.stringify @_objects
@@ -60,14 +59,39 @@ Block.prototype =
 
     return true
 
-BASE = (_prop) ->
+BASE = (_prop, _immutable=false, block=true) ->
   return () ->
-    _obj = @json()
-    _obj[_prop].apply _obj, arguments
+    _obj  = @json()
+    
+    if block is true
+      _args = []
+      for key in Object.keys arguments
+        item = arguments[key]
+        
+        if item.json
+          item = item.json()
+          for index in Object.keys item
+            _args.push item[index]
+        
+        else
+          _args.push item
+    else
+      _args = arguments
+    
+    if _immutable then _obj = _obj[_prop].apply _obj, _args else _obj[_prop].apply _obj, _args
     new Block _obj
 
-for prop in SUPPORT
-  Block.prototype[prop] = BASE prop
+
+for prop in SUPPORT_MUTABLE_BLOCK
+  Block.prototype[prop] = BASE prop, false, true
+
+for prop in SUPPORT_MUTABLE
+  Block.prototype[prop] = BASE prop, false, false
     
+for prop in SUPPORT_IMMUTABLE_BLOCK
+  Block.prototype[prop] = BASE prop, true, true
+
+for prop in SUPPORT_IMMUTABLE
+  Block.prototype[prop] = BASE prop, true, false
 
 module.exports = Block
